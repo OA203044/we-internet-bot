865ccf69f20510847766784678d2d6daa3e08e69
OA203044 <76588271+OA203044@users.noreply.github.com>
2021-03-20 12:58:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,49 +13,49 @@ driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), c
 
 # Now you can start using Selenium
 
+while 2>1:
+  driver.get ('https://my.te.eg/#/home/signin')
+  print(driver.title)
 
-driver.get ('https://my.te.eg/#/home/signin')
-print(driver.title)
-
-time.sleep(3)
-driver.find_element_by_id('MobileNumberID').send_keys('0238728551')
-time.sleep(1)
-driver.find_element_by_id ('PasswordID').send_keys('mni0AMHK')
-time.sleep(1)
-driver.find_element_by_id('singInBtn').click()
-time.sleep(3)
-
-
-# login successful
-print(driver.title)
-
-# getting remainning GB
-array=driver.find_elements_by_css_selector('tspan')
-GB=float(array[3].text)
-time.sleep(3)
-
-#اضغط ع تفاصيل الاستهلاك
-driver.find_element_by_css_selector('button.btn.btn-primary').click()
-time.sleep(3)
-print(driver.title)
+  time.sleep(3)
+  driver.find_element_by_id('MobileNumberID').send_keys('0238728551')
+  time.sleep(1)
+  driver.find_element_by_id ('PasswordID').send_keys('mni0AMHK')
+  time.sleep(1)
+  driver.find_element_by_id('singInBtn').click()
+  time.sleep(3)
 
 
+  # login successful
+  print(driver.title)
 
-#get_attribute("value")
-# تاريخ الشحن ك نص
-date_text= driver.find_element_by_css_selector('div.col-sm-6').text
-# تحويل النص لتاريخ
-date_formatted = datetime.strptime(date_text,"%Y-%m-%d")
-#get current date and time
-now = datetime.now()
-difference = now-date_formatted
-#الايام المتبقية
-days=30-difference.days
-# معدل الاستهلاك ... المعدل الطبيعي 250/30 = 8.33 جيجا في اليوم
-rate=GB/days
-print("%.2f" % rate)
-driver.get ('https://github.com')
+  # getting remainning GB
+  array=driver.find_elements_by_css_selector('tspan')
+  GB=float(array[3].text)
+  time.sleep(3)
 
-time.sleep(2)
+  #اضغط ع تفاصيل الاستهلاك
+  driver.find_element_by_css_selector('button.btn.btn-primary').click()
+  time.sleep(3)
+  print(driver.title)
 
-driver.quit()
+
+
+  #get_attribute("value")
+  # تاريخ الشحن ك نص
+  date_text= driver.find_element_by_css_selector('div.col-sm-6').text
+  # تحويل النص لتاريخ
+  date_formatted = datetime.strptime(date_text,"%Y-%m-%d")
+  #get current date and time
+  now = datetime.now()
+  difference = now-date_formatted
+  #الايام المتبقية
+  days=30-difference.days
+  # معدل الاستهلاك ... المعدل الطبيعي 250/30 = 8.33 جيجا في اليوم
+  rate=GB/days
+  print("%.2f" % rate)
+  driver.get ('https://github.com')
+
+  time.sleep(5)
+
+#driver.quit()
