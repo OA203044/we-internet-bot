6dfd02fc4243d7d8a8c996eefe8db51d4d3ae99d
OA203044 <76588271+OA203044@users.noreply.github.com>
2021-08-18 13:38:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ def job():
   WeLogin()
   SendMail()
 
-WeLogin()
-SendMail() 
   
-scheduler1 = schedule.Scheduler()
-scheduler2 = schedule.Scheduler()
-scheduler3 = schedule.Scheduler()
-scheduler4 = schedule.Scheduler()
+scheduler1 = schedule.Scheduler() #wakeDyno
+scheduler2 = schedule.Scheduler() # send email
+#scheduler3 = schedule.Scheduler()
+#scheduler4 = schedule.Scheduler()
 
 scheduler1.every(28).minutes.do(wakeDyno)
 #scheduler2.every(2).days.at("04:20").do(job) # that will send the email every 2 days @ 6:20 AM Cairo time.
 #scheduler2.every(2).days.at("04:20").do(job)
-scheduler2.every().saturday.at("04:20").do(job) # 6:20 AM Cairo time.
-scheduler3.every().tuesday.at("04:20").do(job)
-scheduler4.every().thursday.at("04:20").do(job)
+scheduler2.every().day.at("04:20").do(job) # 6:20 AM Cairo time.
+#scheduler3.every().tuesday.at("04:20").do(job)
+#scheduler4.every().thursday.at("04:20").do(job)
 
 while True:
   try:
